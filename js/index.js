22d7dc99fa3a17bcf3ec97bad5c1c50530bb36e5
Baroody, Michael <3663076+mbaroody@users.noreply.github.com>
2023-12-15 03:17:53 -0800

    semi good stopping point

--- js/index.js
+++ js/index.js
@@ -1,21 +1,45 @@
-function spanish() {
-    console.log("yooooo!! spanish!!")
+function display(id) {
+    // first, hide main navigation
+    const navigation = document.getElementById("navigation");
+    navigation.classList.add("d-none");
 
-    const info = document.getElementById("info");
-    const spanish = document.getElementById("spanish");
+    // second, show content container
+    const content = document.getElementById("content");
+    content.classList.remove("d-none");
+    content.classList.add("d-inline");
 
-    info.classList.add("d-none");
-    spanish.classList.remove("d-none");
-    spanish.classList.add("d-inline");
+    // third, show panel and add class flag
+    const panel = document.getElementById(id);
+    panel.classList.remove("d-none");
+    panel.classList.add("d-inline");
+    panel.classList.add("current-displayed-panel");
+
+    // last, display, back button to return to main navigation
+    const backButton = document.getElementById("back");
+    backButton.classList.remove("d-none");
+    backButton.classList.add("d-inline");
 }
 
-function english() {
-    console.log("yooooo!! english!!")
+function back() {
+   
+    // first, hide panel and remove class flag
+    const panel = document.getElementsByClassName("current-displayed-panel").item(0);
+    panel.classList.remove("d-inline");
+    panel.classList.remove("current-displayed-panel");
+    panel.classList.add("d-none");
     
-    const info = document.getElementById("info");
-    const english = document.getElementById("english");
+    // second, hide content container
+    const content = document.getElementById("content");
+    content.classList.remove("d-inline");
+    content.classList.add("d-none");
 
-    info.classList.add("d-none");
-    english.classList.remove("d-none");
-    english.classList.add("d-inline");
+    // third, display main navigation
+    const navigation = document.getElementById("navigation");
+    navigation.classList.remove("d-none");
+    navigation.classList.add("d-inline");
+   
+    // last, hide back button to main navigation
+    const backButton = document.getElementById("back");
+    backButton.classList.remove("d-inline");
+    backButton.classList.add("d-none");
 }
